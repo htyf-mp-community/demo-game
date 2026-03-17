810249dbb28446e18237938729cabc4eefc34ba6
dgz <dagouzhi@qq.com>
2026-03-18 00:15:06 +0800

    更新

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,9 +1,21 @@
 extends Node
 @onready var score_label: Label = %ScoreLabel
+@onready var rn_interface: RNInterface = %RNInterface
 
 var score = 0
+var data = null
+
+func _ready() -> void:
+	var msg = 	await  rn_interface.call_rn("getMenuButtonBoundingClientRect", {})
+	if msg == null:
+		data = msg
 
 func add_point(): 
 	score += 1
 	score_label.text = '$ ' + str(score)
 	print(score)
+	
+func get_menu_button_bounding_client_rect() -> void:
+	var msg = 	await  rn_interface.call_rn("getMenuButtonBoundingClientRect", {})
+	if msg == null:
+		data = msg
---
+++ scripts/ui.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+@onready var rn_interface: RNInterface = %RNInterface
+@onready var game_manager: Node = %GameManager
+
+@onready var setting: Button = $Setting
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var data = await game_manager.get_menu_button_bounding_client_rect()
+	if data != null:
+		setting.offset_right = 0
+	pass # Replace with function body.
