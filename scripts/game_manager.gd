717362a59ac8f0722ed697a0bee724bb2091cb18
dgz <dagouzhi@qq.com>
2026-03-19 16:27:47 +0800

    fix

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,26 +2,34 @@ extends Node
 @onready var score_label: Label = %ScoreLabel
 
 var score = 0
-var data = null
+
+# 红糖云小程序中的胶囊菜单定位
+var menuBtonBoundingClientRect := {
+	"top": 0, 
+	"right": 0, 
+	"bottom": 0, 
+	"left": 0, 
+	"width": 0, 
+	"height": 0 
+}
 
 func _ready() -> void:
-	var msg = 	await  HtyfSdk.call_rn("getMenuButtonBoundingClientRect", {})
-	if msg == null:
-		data = msg
+	HtyfSdk.call_get_menu_button_bounding_client_rect(
+		func (result): 
+			#HtyfSdk.call_show_modal("test", JSON.stringify(result))
+			menuBtonBoundingClientRect = result
+	)
+
 
 func add_point(): 
 	score += 1
 	score_label.text = '$ ' + str(score)
 	print(score)
-	
-func get_menu_button_bounding_client_rect() -> void:
-	var msg = 	await  HtyfSdk.call_rn("getMenuButtonBoundingClientRect", {})
-	if msg == null:
-		data = msg
 
 func change_scene(path: String, params := {}) -> void:
+	Engine.time_scale = 1.0
 	var tree := get_tree()
-	#tree.paused = true
+	tree.paused = true
 
 	tree.change_scene_to_file(path)
 	if "init" in params:
--- scripts/ui.gd
+++ scripts/ui.gd
@@ -1,11 +1,24 @@
 extends CanvasLayer
-@onready var game_manager: Node = %GameManager
 
 @onready var setting: Button = $Setting
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var data = await game_manager.get_menu_button_bounding_client_rect()
-	if data != null:
-		setting.offset_right = 0
-	pass # Replace with function body.
+	HtyfSdk.call_get_menu_button_bounding_client_rect(
+		func (result): 
+			var rect = result
+			var size = setting.size
+			# 根据小程序胶囊按钮矩形坐标定位设置按钮。
+			setting.offset_top = rect.get("top", 0)
+			# 设置按钮的左边与胶囊按钮的左边对齐。
+			setting.offset_right = -(rect.get("right", 0) + rect.get("width", 0))
+			if setting.offset_top == 0:
+				setting.offset_top = 20
+			if setting.offset_right == 0:
+				setting.offset_right = -20
+			# HtyfSdk.call_show_modal("rect", JSON.stringify(rect))
+			# HtyfSdk.call_show_modal("offset_top", JSON.stringify(setting.offset_top))
+			print("setting.size.x: "+ str(setting.size.x))
+			print("setting.offset_top: "+ str(setting.offset_top))
+			print("setting.offset_right: "+ str(setting.offset_right))
+	)
